100f220a3079d9629277a50f351a6abd7a901410
tratchapong <tratchapong@gmail.com>
2024-10-26 12:14:28 +0700

    add authRoute + rename middleware

---
+++ routes/auth-route.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const authRoute = express.Router()
+
+authRoute.get('*', ()=>{})
+
+
+module.exports = authRoute
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const cors = require('cors')
-const errorMiddleware = require('./middlewares/errorMiddleware')
-const notFound = require('./middlewares/notFound')
+const errorMiddleware = require('./middlewares/error-middleware')
+const notFound = require('./middlewares/not-found')
 
 const express = require('express')
+const authRoute = require('./routes/auth-route')
 const app = express()
 
 app.use(cors())
+
+app.use('/auth', authRoute)
 
 app.use(notFound)
 app.use(errorMiddleware)
